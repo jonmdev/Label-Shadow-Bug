59f3a31a9a88decb1810fd9b155582380ca651b6
mjondev <43948450+jonmdev@users.noreply.github.com>
2023-10-11 04:02:57 -0400

    simplified

--- Label Shadow Bug/App.xaml.cs
+++ Label Shadow Bug/App.xaml.cs
@@ -17,32 +17,20 @@ namespace Label_Shadow_Bug {
             //LAYOUT
             //=========
             mainPage = new();
-            mainPage.Background = Colors.SaddleBrown;
+            mainPage.Background = Colors.MediumAquamarine;
             MainPage = mainPage;
             mainPage.SizeChanged += delegate {
                 invokeScreenSizeChangeEvent();
             };
 
             AbsoluteLayout abs = new();
-
-            VerticalStackLayout vert = new();
             mainPage.Content = abs;
 
+            VerticalStackLayout vert = new();
             abs.Add(vert);
 
-            NestedBorder nestedBorder = new();
-            nestedBorder = new();
-            nestedBorder.HorizontalOptions = LayoutOptions.Center;
-            nestedBorder.innerBorder.Padding = new Thickness(4, 0);
-            nestedBorder.Margin = new Thickness(0, 4);
-            nestedBorder.BackgroundColor = Colors.DarkGoldenrod;
-            nestedBorder.innerBorder.BackgroundColor = Colors.Coral;
-            nestedBorder.setOuterCornerRadius(12);
-            nestedBorder.setOuterPadding(5);
-            vert.Add(nestedBorder);
-
             Label label = new();
-            label.Text = "HELLO WORLD";
+            label.Text = "CHAT MISSING_WORD";
             label.TextColor = Colors.White;
             label.FontSize = 23; //changing font size can fix bug
             label.FontFamily = "MontserratBold";
@@ -50,8 +38,26 @@ namespace Label_Shadow_Bug {
             label.MaxLines = 1;
             label.LineBreakMode = LineBreakMode.NoWrap;
             label.HorizontalOptions = LayoutOptions.Center;
-            nestedBorder.innerBorder.Content = label;
-            //vert.Children.Add(label); //moving the label to the vert also fixes the bug
+
+            Border newBorder = new();
+            newBorder.HorizontalOptions = LayoutOptions.Center;
+            newBorder.Content = label;
+            newBorder.BackgroundColor = Colors.DarkCyan;
+            newBorder.Padding = new Thickness(6, 2);
+            newBorder.Margin = new Thickness(0, 4);
+            newBorder.StrokeShape = new RoundRectangle() { CornerRadius = 12 };
+            newBorder.StrokeThickness = 0.5;
+            newBorder.HandlerChanged += delegate {
+#if ANDROID
+                Android.Views.View androidView = ElementExtensions.ToPlatform(newBorder, newBorder.Handler.MauiContext);
+                newBorder.Shadow = new() { Offset = new Point(0, androidView.Context.ToPixels(5)), Radius = androidView.Context.ToPixels(4) };
+#else
+                newBorder.Shadow = new() { Offset = new Point(0, 5), Radius = 4 };
+#endif
+            };
+            
+            newBorder.Content = label;
+            vert.Add(newBorder);
 
             Label label2 = new();
             label2.Text = "ALEXANDRA";
@@ -63,13 +69,12 @@ namespace Label_Shadow_Bug {
             label2.HandlerChanged += delegate {
 #if ANDROID
                 Android.Views.View androidView = ElementExtensions.ToPlatform(label2, label2.Handler.MauiContext);
-
                 label2.Shadow = new() { Offset = new Point(0, androidView.Context.ToPixels(5)), Radius = androidView.Context.ToPixels(7) };
+#else
+                label2.Shadow = new() { Offset = new Point(0, 5), Radius = 7 };
 #endif
             };
             vert.Add(label2);
-
-
 
             //==================
             //RESIZE FUNCTION
@@ -90,30 +95,5 @@ namespace Label_Shadow_Bug {
             }
         }
     }
-    public class NestedBorder : Border {
-        public Border innerBorder;
-        Thickness outerPadding;
-        CornerRadius outerCornerRadius;
-        
-        public NestedBorder() {
-            innerBorder = new();
-            this.Content = innerBorder;
-        }
-        public void setOuterPadding(Thickness outerPadding) {
-            this.outerPadding = outerPadding;
-            updatePaddingAndRadiusUniform();
-        }
-        public void setOuterCornerRadius(CornerRadius outerCornerRadius) {
-            this.outerCornerRadius = outerCornerRadius;
-            updatePaddingAndRadiusUniform();
-        }
-        private void updatePaddingAndRadiusUniform() {
-            this.Padding = outerPadding;
-            this.StrokeShape = new RoundRectangle() { CornerRadius = outerCornerRadius };
-            //this.StrokeThickness = 0; //enabling this line fixes bug also
-            innerBorder.StrokeShape = new RoundRectangle() { CornerRadius = (outerCornerRadius.TopLeft - outerPadding.Left) };
-            //innerBorder.StrokeThickness = 0; //enabling this line fixes bug also
-        }
-
-    }
+    
 }
